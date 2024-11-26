f0b4a32aa091970984b865057c37a0238d13fd8c
dingyongya <yahya_dyy@163.com>
2024-11-26 17:10:13 +0800

    第一个组件

---
+++ packages/components/SearchForm/SearchForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>1</div>
+</template>
+
+<script setup lang="ts">
+  defineOptions({
+    name: 'LxSearchForm'
+  })
+</script>
+
+<style lang="" scoped>
+  
+</style>
---
+++ packages/components/SearchForm/index.ts
@@ -0,0 +1,4 @@
+import SearchForm from "./SearchForm.vue";
+import { withInstall } from "@dyy-ui/utils";
+
+export const DySearchForm = withInstall(SearchForm);
